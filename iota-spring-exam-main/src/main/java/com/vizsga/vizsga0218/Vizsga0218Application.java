240dc1121169a2c7d5db38327ccfc5f858c01f79
Daniel Varsas <daniel.varsas@gmail.com>
2021-02-18 16:46:52 +0100

    First

---
+++ iota-spring-exam-main/src/main/java/com/vizsga/vizsga0218/Vizsga0218Application.java
@@ -0,0 +1,13 @@
+package com.vizsga.vizsga0218;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vizsga0218Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vizsga0218Application.class, args);
+	}
+
+}
---
+++ iota-spring-exam-main/src/test/java/com/vizsga/vizsga0218/Vizsga0218ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizsga.vizsga0218;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vizsga0218ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
